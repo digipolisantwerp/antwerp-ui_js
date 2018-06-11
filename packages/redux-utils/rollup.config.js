e20f3783c0db7428059941c05db1d5ab0b98f63f
Tom Opdebeeck <tom.opdebeeck@district01.be>
2018-06-11 14:32:49 +0200

    fix(rollup): add globals & external-helpers

--- packages/redux-utils/rollup.config.js
+++ packages/redux-utils/rollup.config.js
@@ -6,7 +6,7 @@ const rollupHelpers = require("../../rollup.helpers");
 
 export default {
 	input: "src/index.js",
-	output: rollupHelpers.getOutput(packageJson.name, rollupProps.fileName),
-	external: Object.keys(dependencies),
+	output: rollupHelpers.getOutput(packageJson.name, rollupProps.fileName, dependencies),
+	external: rollupHelpers.getExternal(dependencies),
 	plugins: rollupHelpers.getPlugins(),
 };
--- rollup.helpers.js
+++ rollup.helpers.js
@@ -1,40 +1,58 @@
 const babel = require('rollup-plugin-babel');
 const uglify = require('rollup-plugin-uglify');
 const minify = require('uglify-es').minify;
+const camelcase = require('lodash.camelcase');
+const capitalize = require('lodash.capitalize');
 
-const getSingleOuput = (name, fileName, format) => {
+const getSingleOuput = (name, fileName, format, dependencies) => {
     return {
         file: `./dist/${fileName}.${format}.js`,
         format: format,
         name: name,
         sourcemap: true,
-        sourcemapFile: `./dist/${fileName}.${format}.sourcemap.js`,
-    }
-}
+		sourcemapFile: `./dist/${fileName}.${format}.sourcemap.js`,
+		globals: getGlobals(dependencies),
+    };
+};
 
-const getOutput = (name, fileName) => {
+const getOutput = (name, fileName, dependencies) => {
     return [
-        getSingleOuput(name, fileName, "amd"),
-        getSingleOuput(name, fileName, "cjs"),
-        getSingleOuput(name, fileName, "es"),
-        getSingleOuput(name, fileName, "umd"),
-    ]
-}
+        getSingleOuput(name, fileName, "amd", dependencies),
+        getSingleOuput(name, fileName, "cjs", dependencies),
+        getSingleOuput(name, fileName, "es", dependencies),
+        getSingleOuput(name, fileName, "umd", dependencies),
+    ];
+};
 
 const getPlugins = () => {
     return [
         babel({
-            exclude: 'node_modules/**',
+			exclude: 'node_modules/**',
+			plugins: [
+				'external-helpers',
+			],
+			externalHelpers: true,
         }),
         uglify({
             mangle: false
         }, minify),
-    ]
-}
+    ];
+};
 
+const getExternal = (dependencies) => {
+	return Object.keys(dependencies);
+};
+
+const getGlobals = (dependencies) => {
+	return Object.keys(dependencies).reduce((globals, dep) => Object.assign(globals, {
+		[dep]: capitalize(camelcase(dep)),
+	}, {}));
+};
 
 module.exports = {
     getSingleOuput,
     getOutput,
-    getPlugins,
-}
+	getPlugins,
+	getExternal,
+	getGlobals,
+};
