325c8c3ad7df47ed6071017111fd0a01fcd86909
Michael <castiau.m@gmail.com>
2020-01-20 09:24:05 +0100

    Take timezone into accoun to prevent hours offset error

--- packages/date-utils/src/helpers/parseDate.js
+++ packages/date-utils/src/helpers/parseDate.js
@@ -1,6 +1,6 @@
 const moment = require("moment");
 
-export default (d, format = null) => {
+export default (d, format = null, strict = false) => {
 	if (d === undefined || d === null || d instanceof Array) {
 		return null;
 	}
@@ -9,7 +9,7 @@ export default (d, format = null) => {
 		return isNaN(d.valueOf()) ? null : d;
 	}
 
-	const date = format ? moment(d, format, true) : moment(d);
+	const date = format ? moment.utc(d, format, strict) : moment.utc(d);
 
-	return !date.isValid() || date.format() === "Invalid date" ? null : date.toDate();
+	return date.isValid() ? date.toDate() : null;
 };
